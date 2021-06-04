6086d55f7c36daf719482802369bbc6087b0910e
Egor <kirvio@mail.ru>
2021-06-04 09:43:11 +0300

    Create RebootMinistraDocker.py

---
+++ RebootMinistraDocker.py
@@ -0,0 +1,31 @@
+import subprocess
+import time
+
+try:
+    docker_down = subprocess.run(\
+                                 ['docker-compose down'], shell=True, \
+                                 stdout=subprocess.PIPE, universal_newlines=True)
+    print(docker_down.stdout)
+    time.sleep(2)
+    docker_up = subprocess.run(\
+                               ['docker-compose up -d'], check=True, shell=True, \
+                               stdout=subprocess.PIPE, universal_newlines=True)
+    print(docker_up.stdout)
+    time.sleep(2)
+except subprocess.CalledProcessError as err:
+    print(err)
+    pass
+custom = subprocess.run(\
+                        ['sudo cp -L /opt/docker_deploys/ministra/config/custom.ini /opt/docker_deploys/volumes/ministra_web/_data/stalker_portal/server/'], \
+                        check=True, shell=True, stdout=subprocess.PIPE)
+if custom.returncode == 0:
+    print('custom.ini copyed')
+    config = subprocess.run(\
+                            ['sudo cp -L /opt/docker_deploys/ministra/config/config.php /opt/docker_deploys/volumes/ministra_web/_data/stalker_portal/storage/'], \
+                            check=True, shell=True, stdout=subprocess.PIPE)
+    if config.returncode == 0:
+        print('config.php copyed')
+    else:
+        print(config.stderr)
+else:
+    print(custom.stderr)
