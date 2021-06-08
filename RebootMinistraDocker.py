268e54cd64b3d01461fff903c74a6d68e633d262
Egor <kirvio@mail.ru>
2021-06-08 09:32:45 +0300

    Update RebootMinistraDocker.py

--- RebootMinistraDocker.py
+++ RebootMinistraDocker.py
@@ -17,12 +17,12 @@ except subprocess.CalledProcessError as err:
     pass
 custom = subprocess.run(\
                         ['sudo cp -L /opt/docker_deploys/ministra/config/custom.ini /opt/docker_deploys/volumes/ministra_web/_data/stalker_portal/server/'], \
-                        check=True, shell=True, stdout=subprocess.PIPE)
+                        check=True, shell=True, stdout=subprocess.PIPE, universal_newlines=True)
 if custom.returncode == 0:
     print('custom.ini copyed')
     config = subprocess.run(\
                             ['sudo cp -L /opt/docker_deploys/ministra/config/config.php /opt/docker_deploys/volumes/ministra_web/_data/stalker_portal/storage/'], \
-                            check=True, shell=True, stdout=subprocess.PIPE)
+                            check=True, shell=True, stdout=subprocess.PIPE, universal_newlines=True)
     if config.returncode == 0:
         print('config.php copyed')
     else:
