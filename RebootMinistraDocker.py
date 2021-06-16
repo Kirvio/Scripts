ebbd43f4bd1aec20d11d873ead2218b4918e990c
Egor <kirvio@mail.ru>
2021-06-16 13:37:05 +0300

    Update RebootMinistraDocker.py

--- RebootMinistraDocker.py
+++ RebootMinistraDocker.py
@@ -1,4 +1,5 @@
 import subprocess
+import shutil
 import time
 
 try:
@@ -11,21 +12,23 @@ try:
                                ['docker-compose up -d'], check=True, shell=True, \
                                stdout=subprocess.PIPE, universal_newlines=True)
     print(docker_up.stdout)
-    time.sleep(2)
 except subprocess.CalledProcessError as err:
     print(err)
     pass
-custom = subprocess.run(\
-                        ['sudo cp -L /opt/docker_deploys/ministra/config/custom.ini /opt/docker_deploys/volumes/ministra_web/_data/stalker_portal/server/'], \
-                        check=True, shell=True, stdout=subprocess.PIPE, universal_newlines=True)
-if custom.returncode == 0:
-    print('custom.ini copyed')
-    config = subprocess.run(\
-                            ['sudo cp -L /opt/docker_deploys/ministra/config/config.php /opt/docker_deploys/volumes/ministra_web/_data/stalker_portal/storage/'], \
-                            check=True, shell=True, stdout=subprocess.PIPE, universal_newlines=True)
-    if config.returncode == 0:
-        print('config.php copyed')
+finally:
+    try:
+        path_to_custom = '/opt/docker_deploys/ministra/config/custom.ini'
+        path_to_server = '/opt/docker_deploys/volumes/ministra_web/_data/stalker_portal/server/custom.ini'
+        shutil.copyfile(path_to_custom, path_to_server)
+    except (shutil.Error, IOError) as err:
+        print(err)
     else:
-        print(config.stderr)
-else:
-    print(custom.stderr)
+        print('custom.ini copyed')
+        try:
+            path_to_config = '/opt/docker_deploys/ministra/config/config.php'
+            path_to_storage = '/opt/docker_deploys/volumes/ministra_web/_data/stalker_portal/storage/config.php'
+            shutil.copyfile(path_to_config, path_to_storage)
+        except (shutil.Error, IOError) as err:
+            print(err)
+        else:
+            print('config.php copyed')
