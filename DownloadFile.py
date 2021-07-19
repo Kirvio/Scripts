6761b7c768db904489442638dbce6bdde2b63bf0
Egor <kirvio@mail.ru>
2021-07-19 16:30:48 +0300

    Update DownloadFile.py

--- DownloadFile.py
+++ DownloadFile.py
@@ -20,23 +20,46 @@ def Remove(EnterPath, EnterFile):
 
 def Download(url_, path_):
     http = PoolManager()
-    r = http.request('GET', url_, preload_content=False)
+    r = http.request('POST', url_, preload_content=False)
 
-    with open(path_, 'wb') as out:
-        while True:
-            data = r.read(1024)
-            if not data:
-                break
-            out.write(data)
-    print('EPG Downloaded')
-    r.release_conn()
+    _recv_buffer = b''
+    result = False
+    try:
+        with open(path_, 'wb') as out:
+            while True:
+                try:
+                    data = r.read(2048)
+                    if not data:
+                        break
+                except (Exception, IOError) as exc:
+                    print(exc)
+                    time.sleep(20)
+                    break
+                else:
+                    out.write(_recv_buffer)
+    except (Exception, OSError) as exc:
+        print(exc)
+        time.sleep(20)
+    else:
+        print('Архив с EPG загружен')
+        result = True
+    finally:
+        r.release_conn()
+        yield result
 
 def Extract_archive(url_, download_path, file_path):
-    Download(url_, download_path)
-    with gzip.open(download_path, 'rb') as archive_:
-        with open(file_path, 'wb') as file_:
-            shutil.copyfileobj(archive_, file_)
-            print('all done')
+    if next(Download(url_, download_path)) is True:
+        try:
+            with gzip.open(download_path, 'rb') as archive_,\
+                    open(file_path, 'wb') as file_:
+                        shutil.copyfileobj(archive_, file_)
+        except (Exception, OSError) as exc:
+            print(exc)
+            time.sleep(20)
+        else:
+            print('Файл с EPG извлечён из архива')
+    else:
+        print('Ошибка при загрузке файла с ЕПГ')
 
 def MainFunction():
     try:
@@ -44,36 +67,50 @@ def MainFunction():
         full_path = '\\'.join([epg_path, 'epg.xml'])
         exist_file = os.path.isfile(full_path)
         result = False
+    except (Exception, OSError) as exc:
+        print(exc)
+        time.sleep(20)
+    else:
         if exist_archive and exist_file:
-            Remove(archive_path, EnterFile = 'epg.xml.gz')
-            [os.remove(file.path) for file in os.scandir(epg_path)]
-            time.sleep(0.3)
-            Extract_archive(url, archive_path, full_path)
-            result = True
+            try:
+                Remove(archive_path, EnterFile = 'epg.xml.gz')
+                [os.remove(file.path) for file in os.scandir(epg_path)]
+                time.sleep(1)
+                Extract_archive(url, archive_path, full_path)
+            except (Exception, OSError) as exc:
+                print(exc)
+                time.sleep(20)
+            else:
+                result = True
         elif exist_file and not exist_archive:
-            [os.remove(file.path) for file in os.scandir(epg_path)]
-            Extract_archive(url, archive_path, full_path)
-            time.sleep(0.3)
-            result = True
+            try:
+                [os.remove(file.path) for file in os.scandir(epg_path)]
+                Extract_archive(url, archive_path, full_path)
+                time.sleep(1)
+            except (Exception, OSError) as exc:
+                print(exc)
+                time.sleep(20)
+            else:
+                result = True
         elif exist_archive and not exist_file:
-            Remove(archive_path, EnterFile = 'epg.xml.gz')
-            time.sleep(0.3)
-            Extract_archive(url, archive_path, full_path)
-            result = True
+            try:
+                Remove(archive_path, EnterFile = 'epg.xml.gz')
+                time.sleep(1)
+                Extract_archive(url, archive_path, full_path)
+            except (Exception, OSError) as exc:
+                print(exc)
+                time.sleep(20)
+            else:
+                result = True
         else:
-            print('not at all')
             Extract_archive(url, archive_path, full_path)
             result = True
-    except (Exception, OSError) as err:
-        print(err)
-        result = False
-        raise
     finally:
-        return result
+        yield result
 
 if __name__ == '__main__':
     MF = MainFunction()
-    if MF:
+    if next(MF) is True:
         os.startfile('D:/Network/КИТ Интернет телепрограмма/КИТ Интернет телепрограмма.exe')
-    else: 
+    else:
         print('Ошибка')
