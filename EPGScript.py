ffde112d7ab8503705bfea42fb4c2001e80ec03f
Egor <kirvio@mail.ru>
2021-07-28 13:38:58 +0300

    Update EPGScript.py

--- EPGScript.py
+++ EPGScript.py
@@ -1,70 +1,93 @@
-import paramiko 
+import logging
 import time
+import sys
 import os
+try:
+    import paramiko
+except ImportError as exc:
+    print(exc)
+    time.sleep(20)
 
-"""This script is used to transport ready EPG.xmltv file
-   to ministra in our server
+"""
+Скрипт закидывает с хоста (сначала на сервер,
+потом в министру) готовый xmltv.xml файл
+по ssh. Для передачи файлов используется
+paramiko.Transport(sftp), для подключения
+по ssh paramiko.SSHClient()
 """
 
-host = ''
-user = ''
-secret = ''
-port = 22
 remotepath = '/home/evandrushenko/xmltv.xml'
-localpath = 'D:\\EPG\\xmltv.xml'
+localpath = 'D:/EPG/xmltv.xml'
 
 # Передача xmltv файла по sftp на сервер
-def TransportData(frm, to):
+def TransportData(frm, to, host='172.16.100.54', port=22,\
+                  SSHuser='evandrushenko', SSHsecret='1234567Andr'):
+    result = False
     kak = os.path.isfile(localpath)
     if kak:
         try:
             transport = paramiko.Transport((host, port))
-            transport.connect(username = user, password = secret)
+            transport.connect(username=SSHuser, password=SSHsecret)
             sftp = paramiko.SFTPClient.from_transport(transport)
+
             # Передача файлов
             sftp.put(frm, to)
-            return True
         except (paramiko.SSHException, OSError) as err:
             print(err)
-            return err
-        finally:
+            time.sleep(20)
+        else:
             sftp.close()
             transport.close()
+            log.info("xml файл с EPG на сервере")
+            result = True
+        finally:
+            yield result
     else:
         print("xmltv файл отсутствует!")
         time.sleep(10)
-        return False
 
 # Соединение с сервером по ssh и копирование (sudo) с директории в директорию с министрой
-def SSHConnect(SHhost, SHuser, SHpassword, SHport):
+def SSHConnect(SHhost='172.16.100.54', SHport=22,\
+               SHuser='evandrushenko', SHpassword='1234567Andr'):
     try:
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(hostname = SHhost, username = SHuser, password = SHpassword, port = SHport)
+        ssh.connect(hostname=SHhost, username=SHuser, \
+                    password=SHpassword, port=SHport)
 
         chan = ssh.get_transport().open_session()
         chan.get_pty()
 
-        # channel.settimeout(5)
         cmd = 'sudo cp -L /home/evandrushenko/xmltv.xml /opt/docker_deploys/volumes/ministra_epg/_data/'
         chan.exec_command(cmd)
-        chan.send(secret + '\n')
-        
-        time.sleep(3)
-        return chan.recv(1024)
+        chan.send(SHpassword + '\n')
+
+        time.sleep(5)
     except (paramiko.SSHException, OSError) as err:
         print(err)
-        return err
-    finally:
-        #chan.close()
+        time.sleep(20)
+    else:
         ssh.close()
+        log.info("Файл перенесён в докер")
 
 if __name__ == '__main__': 
-    ST = TransportData(frm = localpath, to = remotepath)
-    print(ST)
-    time.sleep(2)
-    if ST:
-        SH = SSHConnect(SHhost = host, SHuser = user, SHpassword = secret, SHport = port)    
-        print(SH)
+    try:
+        log = logging.getLogger(__name__)
+        log.setLevel(logging.INFO)
+        f_format = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(logging.INFO)
+        handler.setFormatter(f_format)
+        log.addHandler(handler)
+    except Exception as exc:
+        print(exc)
+        time.sleep(20)
     else:
-        print("Скрипт не выполнился")
+        ST = TransportData(frm=localpath, to=remotepath)
+        time.sleep(2)
+        if next(ST):
+            SH = SSHConnect()
+            time.sleep(2)
+        else:
+            log.info("Скрипт не выполнился")
+            time.sleep(2)
