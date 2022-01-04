dd9b584f2ae54bea74d7ccd3ae37f9de713fe5c9
Egor <kirvio@mail.ru>
2022-01-04 16:00:34 +0300

    Update reload_bind.sh

--- renew_parser_belgie/modules/reload_bind.sh
+++ renew_parser_belgie/modules/reload_bind.sh
@@ -4,11 +4,11 @@ DIR="/home/belgie/new_parser_belgie/zones.blacklisted /etc/bind/zones/"
 RELOAD="service bind9 reload"
 
 function main_function {
-    /bin/cp $DIR 2> /home/belgie/new_parser_belgie/belgie_logs/rel_bind.log
+    /bin/cp $DIR &> /home/belgie/new_parser_belgie/belgie_logs/rel_bind.log
     if [ $? -eq 0 ]; then
-        /usr/sbin/named-checkconf 2> /home/belgie/new_parser_belgie/belgie_logs/rel_bind.log
+        /usr/sbin/named-checkconf &> /home/belgie/new_parser_belgie/belgie_logs/rel_bind.log
         if [ $? -eq 0 ]; then
-	    ${RELOAD} 2> /home/belgie/new_parser_belgie/belgie_logs/rel_bind.log
+	    ${RELOAD} &> /home/belgie/new_parser_belgie/belgie_logs/rel_bind.log
 	    if [ $? -eq 0 ]; then
                 exit 0
             else
